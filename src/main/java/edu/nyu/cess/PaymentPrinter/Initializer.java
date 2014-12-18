f5db873ece1f0009ee97f0d084b748fbdf83e23d
Anwar A. Ruff <anwarruff@gmail.com>
2014-12-18 14:17:15 -0500

    resource moved to correspond to Maven's project folder hierarchy

--- src/main/java/edu/nyu/cess/PaymentPrinter/Initializer.java
+++ src/main/java/edu/nyu/cess/PaymentPrinter/Initializer.java
@@ -1,9 +1,6 @@
 package edu.nyu.cess.PaymentPrinter;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
+import javax.swing.*;
 
 public class Initializer {
 	
