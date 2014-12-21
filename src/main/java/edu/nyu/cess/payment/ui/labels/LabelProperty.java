f3adecb4aa2b093378d36caaed2dbf37a1cb399e
Anwar A. Ruff <anwarruff@gmail.com>
2014-12-20 23:37:39 -0500

    Created Label Property

---
+++ src/main/java/edu/nyu/cess/payment/ui/labels/LabelProperty.java
@@ -0,0 +1,44 @@
+package edu.nyu.cess.payment.ui.labels;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * The type Label property.
+ */
+public abstract class LabelProperty
+{
+    protected String labelText;
+    protected Color fontColor;
+    protected ImageIcon imageIcon;
+
+    /**
+     * Gets default font color.
+     *
+     * @return the default font color
+     */
+    public Color getFontColor()
+    {
+        return fontColor;
+    }
+
+    /**
+     * Gets info icon.
+     *
+     * @return the info icon
+     */
+    public ImageIcon getImageIcon()
+    {
+        return imageIcon;
+    }
+
+    /**
+     * Gets label text.
+     *
+     * @return the label text
+     */
+    public String getLabelText()
+    {
+        return labelText;
+    }
+}
