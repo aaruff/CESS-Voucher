107879adf62e2f68003a4f2a1533fc1d786c3212
Anwar A. Ruff <anwarruff@gmail.com>
2014-12-20 23:37:09 -0500

    Created Error Label Property

---
+++ src/main/java/edu/nyu/cess/payment/ui/labels/ErrorLabelProperty.java
@@ -0,0 +1,18 @@
+package edu.nyu.cess.payment.ui.labels;
+
+import edu.nyu.cess.payment.io.IconFactory;
+
+import java.awt.*;
+
+public class ErrorLabelProperty extends LabelProperty
+{
+    private final String iconPath = "/images/error.png";
+    private final String iconDescription = "Error";
+
+    public ErrorLabelProperty(String message)
+    {
+        labelText = message;
+        imageIcon = IconFactory.createImageIcon(iconPath, iconDescription);
+        fontColor =  Color.RED;
+    }
+}
