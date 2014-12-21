fc4f9a488db799d1feed15abf7274722905c37a9
Anwar A. Ruff <anwarruff@gmail.com>
2014-12-20 23:36:20 -0500

    Created Open File Button

---
+++ src/main/java/edu/nyu/cess/payment/ui/buttons/OpenFileButton.java
@@ -0,0 +1,21 @@
+package edu.nyu.cess.payment.ui.buttons;
+
+import edu.nyu.cess.payment.io.IconFactory;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class OpenFileButton extends JButton
+{
+    private String iconPath = "/images/file_open.png";
+    private String iconDescription = "Open File";
+
+    public OpenFileButton(ActionListener actionListener)
+    {
+        setText("Open File");
+        setPreferredSize(new Dimension(190, 40));
+        setIcon(IconFactory.createImageIcon(iconPath, iconDescription));
+        addActionListener(actionListener);
+    }
+}
