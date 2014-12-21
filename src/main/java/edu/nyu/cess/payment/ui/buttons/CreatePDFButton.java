a70cdfc64de52260be0cec99dce15710e7e2b80f
Anwar A. Ruff <anwarruff@gmail.com>
2014-12-20 23:36:07 -0500

    Created PDF Button

---
+++ src/main/java/edu/nyu/cess/payment/ui/buttons/CreatePDFButton.java
@@ -0,0 +1,21 @@
+package edu.nyu.cess.payment.ui.buttons;
+
+import edu.nyu.cess.payment.io.IconFactory;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class CreatePDFButton extends JButton
+{
+    private String iconPath = "/images/pdf_ico.png";
+    private String iconDescription = "PDF";
+
+    public CreatePDFButton(ActionListener actionListener)
+    {
+        setText("Convert to Voucher PDF");
+        setPreferredSize(new Dimension(190, 40));
+        setIcon(IconFactory.createImageIcon(iconPath, iconDescription));
+        addActionListener(actionListener);
+    }
+}
