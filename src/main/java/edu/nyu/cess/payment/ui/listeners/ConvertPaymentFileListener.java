84c7eba460fe4a34c4c005dfebdf81791084d067
Anwar A. Ruff <anwarruff@gmail.com>
2014-12-21 23:08:17 -0500

    Add comments

--- src/main/java/edu/nyu/cess/payment/ui/listeners/ConvertPaymentFileListener.java
+++ src/main/java/edu/nyu/cess/payment/ui/listeners/ConvertPaymentFileListener.java
@@ -1,6 +1,7 @@
 package edu.nyu.cess.payment.ui.listeners;
 
 import edu.nyu.cess.payment.io.FileConverter;
+import edu.nyu.cess.payment.ui.IconFactory;
 
 import javax.swing.*;
 import java.awt.*;
@@ -14,22 +15,25 @@ public class ConvertPaymentFileListener implements ActionListener
     private static final String ERROR_ICON_PATH = "/images/error.png";
 
     private FileConverter fileConverter;
+    private ImageIcon errorIcon;
 
     public ConvertPaymentFileListener(FileConverter fileConverter)
     {
         this.fileConverter = fileConverter;
+        errorIcon = IconFactory.createImageIcon(ERROR_ICON_PATH, "error");
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        // Error: File Not Set
         if(!fileConverter.isFileInfoSet()){
             statusLabel.setForeground(Color.RED);
-            ImageIcon errorIcon = createImageIcon("/resources/images/error.png", "error");
             statusLabel.setIcon(errorIcon);
             statusLabel.setText("Error: Please select \"Open File\" first.");
             return;
         }
 
+        // Error: Failed To Covert File
         if(!fileConverter.convertPaymentToVoucherPDF(horizontalShift, verticalShift)){
             ImageIcon errorIcon = createImageIcon("/resources/images/error.png", "error");
             statusLabel.setIcon(errorIcon);
@@ -42,14 +46,15 @@ public class ConvertPaymentFileListener implements ActionListener
             try {
                 File myFile = new File(fileConverter.getPDFLocation());
                 Desktop.getDesktop().open(myFile);
-            } catch (IOException ex) {
+            }
+            // Error: Unable to Open File
+            catch (IOException ex) {
                 ImageIcon errorIcon = createImageIcon("/resources/images/error.png", "error");
                 statusLabel.setIcon(errorIcon);
                 statusLabel.setForeground(Color.RED);
                 statusLabel.setText("Error: Unable to auto open File ("+ fileConverter.getPDFName()+".pdf)");
             }
         }
-
 
         ImageIcon infoIcon = createImageIcon("/resources/images/info.png", "info");
         statusLabel.setIcon(infoIcon);
