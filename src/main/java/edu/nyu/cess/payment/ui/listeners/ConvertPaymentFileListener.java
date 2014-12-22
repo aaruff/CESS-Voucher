625641c0906a8a0b11c9e4d676d2905f6c27c361
Anwar A. Ruff <anwarruff@gmail.com>
2014-12-22 16:39:41 -0500

    Updated to reflect renamed classes

--- src/main/java/edu/nyu/cess/payment/ui/listeners/ConvertPaymentFileListener.java
+++ src/main/java/edu/nyu/cess/payment/ui/listeners/ConvertPaymentFileListener.java
@@ -1,66 +1,66 @@
 package edu.nyu.cess.payment.ui.listeners;
 
-import edu.nyu.cess.payment.io.FileConverter;
-import edu.nyu.cess.payment.ui.IconFactory;
+import edu.nyu.cess.payment.io.ZTreePayFileConverter;
+import edu.nyu.cess.payment.ui.ZTreePayoffFileConversionPanel;
+import edu.nyu.cess.payment.ui.StatusType;
 
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * The type Convert payment file listener.
+ */
 public class ConvertPaymentFileListener implements ActionListener
 {
-    private static final String ERROR_ICON_PATH = "/images/error.png";
+    private ZTreePayoffFileConversionPanel ZTreePayoffFileConversionPanel;
 
-    private FileConverter fileConverter;
-    private ImageIcon errorIcon;
+    private ZTreePayFileConverter zTreePayFileConverter = new ZTreePayFileConverter();
 
-    public ConvertPaymentFileListener(FileConverter fileConverter)
+    /**
+     * Instantiates a new Convert payment file listener.
+     *
+     * @param ZTreePayoffFileConversionPanel the converter panel
+     */
+    public ConvertPaymentFileListener(ZTreePayoffFileConversionPanel ZTreePayoffFileConversionPanel)
     {
-        this.fileConverter = fileConverter;
-        errorIcon = IconFactory.createImageIcon(ERROR_ICON_PATH, "error");
+        this.ZTreePayoffFileConversionPanel = ZTreePayoffFileConversionPanel;
     }
 
+    /**
+     * Action performed.
+     *
+     * @param e the e
+     */
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e)
+    {
         // Error: File Not Set
-        if(!fileConverter.isFileInfoSet()){
-            statusLabel.setForeground(Color.RED);
-            statusLabel.setIcon(errorIcon);
-            statusLabel.setText("Error: Please select \"Open File\" first.");
+        if( ! zTreePayFileConverter.isFileInfoSet()){
+            ZTreePayoffFileConversionPanel.updateStatus(StatusType.ERROR, "Error: The file payoff file must be selected first.");
             return;
         }
 
         // Error: Failed To Covert File
-        if(!fileConverter.convertPaymentToVoucherPDF(horizontalShift, verticalShift)){
-            ImageIcon errorIcon = createImageIcon("/resources/images/error.png", "error");
-            statusLabel.setIcon(errorIcon);
-            statusLabel.setForeground(Color.RED);
-            statusLabel.setText("Error: Failed to convert payment file, check format.");
+        if( ! zTreePayFileConverter.convertPaymentToVoucherPDF()){
+            ZTreePayoffFileConversionPanel.updateStatus(StatusType.ERROR, "Error: Failed to convert payment file. Please check the format.");
             return;
         }
 
         if (Desktop.isDesktopSupported()) {
             try {
-                File myFile = new File(fileConverter.getPDFLocation());
+                File myFile = new File(zTreePayFileConverter.getPDFLocation());
                 Desktop.getDesktop().open(myFile);
             }
             // Error: Unable to Open File
             catch (IOException ex) {
-                ImageIcon errorIcon = createImageIcon("/resources/images/error.png", "error");
-                statusLabel.setIcon(errorIcon);
-                statusLabel.setForeground(Color.RED);
-                statusLabel.setText("Error: Unable to auto open File ("+ fileConverter.getPDFName()+".pdf)");
+                ZTreePayoffFileConversionPanel.updateStatus(StatusType.ERROR, "Error: Unable to auto open selected file.");
             }
         }
 
-        ImageIcon infoIcon = createImageIcon("/resources/images/info.png", "info");
-        statusLabel.setIcon(infoIcon);
-        statusLabel.setText("");
-        statusLabel.setText("Output File: " + fileConverter.getPDFName()+".pdf");
+        ZTreePayoffFileConversionPanel.updateStatus(StatusType.INFO, "Output File: " + zTreePayFileConverter.getPDFName() + ".pdf");
     }
-
 }
 
