a6c79ddc23aec071f9242d0b75b1dd2f5d8d6eea
Anwar A. Ruff <anwarruff@gmail.com>
2014-12-24 23:41:56 -0500

    Created InvalidFileSelectedException

---
+++ src/main/java/edu/nyu/cess/payment/exceptions/InvalidFileSelectedException.java
@@ -0,0 +1,8 @@
+package edu.nyu.cess.payment.exceptions;
+
+public class InvalidFileSelectedException extends Exception
+{
+    public InvalidFileSelectedException(String message){
+        super(message);
+    }
+}
