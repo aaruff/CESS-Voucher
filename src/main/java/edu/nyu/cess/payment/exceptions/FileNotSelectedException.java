ac7a0e7df09a855e4567dbe90ecccdf270094c39
Anwar A. Ruff <anwarruff@gmail.com>
2014-12-26 23:48:11 -0500

    Added exception descriptions (doc block)

--- src/main/java/edu/nyu/cess/payment/exceptions/FileNotSelectedException.java
+++ src/main/java/edu/nyu/cess/payment/exceptions/FileNotSelectedException.java
@@ -1,5 +1,8 @@
 package edu.nyu.cess.payment.exceptions;
 
+/**
+ * The Exception that is thrown when I file has not been selected.
+ */
 public class FileNotSelectedException extends Exception
 {
     public FileNotSelectedException(String message)
--- src/main/java/edu/nyu/cess/payment/exceptions/InvalidFileSelectedException.java
+++ src/main/java/edu/nyu/cess/payment/exceptions/InvalidFileSelectedException.java
@@ -1,5 +1,8 @@
 package edu.nyu.cess.payment.exceptions;
 
+/**
+ * The exception that occurs when an invalid file is selected.
+ */
 public class InvalidFileSelectedException extends Exception
 {
     public InvalidFileSelectedException(String message){
