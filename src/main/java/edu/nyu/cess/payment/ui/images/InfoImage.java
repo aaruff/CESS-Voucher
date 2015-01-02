48d82618d277393ef60364195c80f7e415bd5601
Anwar A. Ruff <anwarruff@gmail.com>
2015-01-01 23:30:13 -0500

    Created Info image class

---
+++ src/main/java/edu/nyu/cess/payment/ui/images/InfoImage.java
@@ -0,0 +1,7 @@
+package edu.nyu.cess.payment.ui.images;
+
+public class InfoImage extends IconImage
+{
+    private String path = "/images/info.png";
+    private String description =  "Information";
+}
