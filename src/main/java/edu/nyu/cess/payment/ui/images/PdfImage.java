e52d32afb7a7428b67f4193a4727ef957c265e59
Anwar A. Ruff <anwarruff@gmail.com>
2015-01-01 23:30:37 -0500

    Created PDF image class

---
+++ src/main/java/edu/nyu/cess/payment/ui/images/PdfImage.java
@@ -0,0 +1,10 @@
+package edu.nyu.cess.payment.ui.images;
+
+/**
+ * The type Pdf image.
+ */
+public class PdfImage extends IconImage
+{
+    private String path = "/images/cess.png";
+    private String description =  "CESS";
+}
