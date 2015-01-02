22a7ed56385550f1edbafbea3e6b96d2d9f29c86
Anwar A. Ruff <anwarruff@gmail.com>
2015-01-01 23:30:30 -0500

    Created Open File image class

---
+++ src/main/java/edu/nyu/cess/payment/ui/images/OpenFileImage.java
@@ -0,0 +1,10 @@
+package edu.nyu.cess.payment.ui.images;
+
+/**
+ * The type Open file image.
+ */
+public class OpenFileImage extends IconImage
+{
+    private String path = "/images/file_open.png";
+    private String description = "Open File";
+}
