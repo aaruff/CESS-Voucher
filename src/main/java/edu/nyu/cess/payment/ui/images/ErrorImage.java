2d3c7054c50319e85837393af7408a9017373c11
Anwar A. Ruff <anwarruff@gmail.com>
2015-01-01 23:29:44 -0500

    Created Error image class

---
+++ src/main/java/edu/nyu/cess/payment/ui/images/ErrorImage.java
@@ -0,0 +1,7 @@
+package edu.nyu.cess.payment.ui.images;
+
+public class ErrorImage extends IconImage
+{
+    private final String path = "/images/error.png";
+    private final String description = "Error";
+}
