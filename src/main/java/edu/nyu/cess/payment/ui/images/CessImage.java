318810773a2051270570900dac7fa322b3931583
Anwar A. Ruff <anwarruff@gmail.com>
2015-01-01 23:29:32 -0500

    Created CESS image class

---
+++ src/main/java/edu/nyu/cess/payment/ui/images/CessImage.java
@@ -0,0 +1,10 @@
+package edu.nyu.cess.payment.ui.images;
+
+/**
+ * The type CESS image.
+ */
+public class CessImage extends IconImage
+{
+    private String path = "/images/cess.png";
+    private String description = "CESS";
+}
