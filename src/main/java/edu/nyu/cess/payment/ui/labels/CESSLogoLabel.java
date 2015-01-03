b2183e0b9629b600617c25580aed0daec7be52d4
Anwar A. Ruff <anwarruff@gmail.com>
2015-01-03 13:54:02 -0500

    Removed duplicate text

--- src/main/java/edu/nyu/cess/payment/ui/labels/CESSLogoLabel.java
+++ src/main/java/edu/nyu/cess/payment/ui/labels/CESSLogoLabel.java
@@ -9,7 +9,6 @@ public class CESSLogoLabel extends JLabel
 {
     public CESSLogoLabel()
     {
-        setText("CESS Lab Toolkit");
         setIcon(IconFactory.produceCessIcon());
         setFont(new Font("Helvetica", Font.BOLD, 28));
         setForeground(new Color(19, 19, 33));
