23414b6a05f09afb30a409335f22fdca5e4f9077
Anwar A. Ruff <anwarruff@gmail.com>
2015-01-09 23:49:33 -0500

    Added doc blocks

--- src/main/java/edu/nyu/cess/experiment/payment/voucher/Voucher.java
+++ src/main/java/edu/nyu/cess/experiment/payment/voucher/Voucher.java
@@ -2,15 +2,28 @@ package edu.nyu.cess.experiment.payment.voucher;
 
 import java.util.ArrayList;
 
+/**
+ * The type Voucher.
+ */
 public class Voucher
 {
     private ArrayList<Item> items = new ArrayList<Item>();
 
+    /**
+     * Add item.
+     *
+     * @param item the item
+     */
     public void addItem(Item item)
     {
         items.add(item);
     }
 
+    /**
+     * Gets items.
+     *
+     * @return the items
+     */
     public ArrayList<Item> getItems()
     {
         return this.items;
