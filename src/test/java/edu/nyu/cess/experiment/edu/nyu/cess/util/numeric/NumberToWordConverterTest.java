c2962cccd8c8adf5042d59c4aef57c3acb6b707c
Anwar A. Ruff <anwarruff@gmail.com>
2015-01-17 23:19:49 -0500

    Added unit test for method fractionalPortionInWords

--- src/test/java/edu/nyu/cess/experiment/edu/nyu/cess/util/numeric/NumberToWordConverterTest.java
+++ src/test/java/edu/nyu/cess/experiment/edu/nyu/cess/util/numeric/NumberToWordConverterTest.java
@@ -4,69 +4,74 @@ import junit.framework.TestCase;
 
 public class NumberToWordConverterTest extends TestCase
 {
+    public void testFractionalNumberToWords()
+    {
+        assertEquals("Ninety Nine", NumberToWordConverter.fractionalPortionInWords(100000.99));
+        assertEquals("Ninety Nine", NumberToWordConverter.fractionalPortionInWords(0.99));
+    }
     public void testWholeNumberToWords()
     {
-        assertTrue(NumberToWordConverter.wholePortionInWords(100000.99).equals(""));
-        assertTrue(NumberToWordConverter.wholePortionInWords(0.99).equals(""));
+        assertEquals("", NumberToWordConverter.wholePortionInWords(100000.99));
+        assertEquals("", NumberToWordConverter.wholePortionInWords(0.99));
 
-        assertTrue(NumberToWordConverter.wholePortionInWords(900.99).equals("Nine Hundred"));
-        assertTrue(NumberToWordConverter.wholePortionInWords(800.99).equals("Eight Hundred"));
-        assertTrue(NumberToWordConverter.wholePortionInWords(700.99).equals("Seven Hundred"));
-        assertTrue(NumberToWordConverter.wholePortionInWords(600.99).equals("Six Hundred"));
-        assertTrue(NumberToWordConverter.wholePortionInWords(500.99).equals("Five Hundred"));
-        assertTrue(NumberToWordConverter.wholePortionInWords(400.99).equals("Four Hundred"));
-        assertTrue(NumberToWordConverter.wholePortionInWords(300.99).equals("Three Hundred"));
-        assertTrue(NumberToWordConverter.wholePortionInWords(200.99).equals("Two Hundred"));
-        assertTrue(NumberToWordConverter.wholePortionInWords(100.99).equals("One Hundred"));
-        assertTrue(NumberToWordConverter.wholePortionInWords(900.99).equals("Nine Hundred"));
+        assertEquals("Nine Hundred", NumberToWordConverter.wholePortionInWords(900.99));
+        assertEquals("Eight Hundred", NumberToWordConverter.wholePortionInWords(800.99));
+        assertEquals("Seven Hundred", NumberToWordConverter.wholePortionInWords(700.99));
+        assertEquals("Six Hundred", NumberToWordConverter.wholePortionInWords(600.99));
+        assertEquals("Five Hundred", NumberToWordConverter.wholePortionInWords(500.99));
+        assertEquals("Four Hundred", NumberToWordConverter.wholePortionInWords(400.99));
+        assertEquals("Three Hundred", NumberToWordConverter.wholePortionInWords(300.99));
+        assertEquals("Two Hundred", NumberToWordConverter.wholePortionInWords(200.99));
+        assertEquals("One Hundred", NumberToWordConverter.wholePortionInWords(100.99));
+        assertEquals("Nine Hundred", NumberToWordConverter.wholePortionInWords(900.99));
 
-        assertTrue(NumberToWordConverter.wholePortionInWords(990.99).equals("Nine Hundred Ninety"));
+        assertEquals("Nine Hundred Ninety", NumberToWordConverter.wholePortionInWords(990.99));
     }
 
     public void testBaseTenPositionToWords()
     {
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(9, -1).equals(""));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(9, 100000).equals(""));
+        assertEquals("", NumberToWordConverter.baseTenMultipleToWords(9, -1));
+        assertEquals("", NumberToWordConverter.baseTenMultipleToWords(9, 100000));
 
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(1, 0).equals("One"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(2, 0).equals("Two"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(3, 0).equals("Three"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(4, 0).equals("Four"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(5, 0).equals("Five"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(6, 0).equals("Six"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(7, 0).equals("Seven"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(8, 0).equals("Eight"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(9, 0).equals("Nine"));
+        assertEquals("One", NumberToWordConverter.baseTenMultipleToWords(1, 0));
+        assertEquals("Two", NumberToWordConverter.baseTenMultipleToWords(2, 0));
+        assertEquals("Three", NumberToWordConverter.baseTenMultipleToWords(3, 0));
+        assertEquals("Four", NumberToWordConverter.baseTenMultipleToWords(4, 0));
+        assertEquals("Five", NumberToWordConverter.baseTenMultipleToWords(5, 0));
+        assertEquals("Six", NumberToWordConverter.baseTenMultipleToWords(6, 0));
+        assertEquals("Seven", NumberToWordConverter.baseTenMultipleToWords(7, 0));
+        assertEquals("Eight", NumberToWordConverter.baseTenMultipleToWords(8, 0));
+        assertEquals("Nine", NumberToWordConverter.baseTenMultipleToWords(9, 0));
 
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(1, 1).equals("Ten"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(2, 1).equals("Twenty"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(3, 1).equals("Thirty"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(4, 1).equals("Forty"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(5, 1).equals("Fifty"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(6, 1).equals("Sixty"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(7, 1).equals("Seventy"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(8, 1).equals("Eighty"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(9, 1).equals("Ninety"));
+        assertEquals("Ten", NumberToWordConverter.baseTenMultipleToWords(1, 1));
+        assertEquals("Twenty", NumberToWordConverter.baseTenMultipleToWords(2, 1));
+        assertEquals("Thirty", NumberToWordConverter.baseTenMultipleToWords(3, 1));
+        assertEquals("Forty", NumberToWordConverter.baseTenMultipleToWords(4, 1));
+        assertEquals("Fifty", NumberToWordConverter.baseTenMultipleToWords(5, 1));
+        assertEquals("Sixty", NumberToWordConverter.baseTenMultipleToWords(6, 1));
+        assertEquals("Seventy", NumberToWordConverter.baseTenMultipleToWords(7, 1));
+        assertEquals("Eighty", NumberToWordConverter.baseTenMultipleToWords(8, 1));
+        assertEquals("Ninety", NumberToWordConverter.baseTenMultipleToWords(9, 1));
 
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(1, 2).equals("One Hundred"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(2, 2).equals("Two Hundred"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(3, 2).equals("Three Hundred"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(4, 2).equals("Four Hundred"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(5, 2).equals("Five Hundred"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(6, 2).equals("Six Hundred"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(7, 2).equals("Seven Hundred"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(8, 2).equals("Eight Hundred"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(9, 2).equals("Nine Hundred"));
+        assertEquals("One Hundred", NumberToWordConverter.baseTenMultipleToWords(1, 2));
+        assertEquals("Two Hundred", NumberToWordConverter.baseTenMultipleToWords(2, 2));
+        assertEquals("Three Hundred", NumberToWordConverter.baseTenMultipleToWords(3, 2));
+        assertEquals("Four Hundred", NumberToWordConverter.baseTenMultipleToWords(4, 2));
+        assertEquals("Five Hundred", NumberToWordConverter.baseTenMultipleToWords(5, 2));
+        assertEquals("Six Hundred", NumberToWordConverter.baseTenMultipleToWords(6, 2));
+        assertEquals("Seven Hundred", NumberToWordConverter.baseTenMultipleToWords(7, 2));
+        assertEquals("Eight Hundred", NumberToWordConverter.baseTenMultipleToWords(8, 2));
+        assertEquals("Nine Hundred", NumberToWordConverter.baseTenMultipleToWords(9, 2));
 
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(1, 3).equals("One Thousand"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(2, 3).equals("Two Thousand"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(3, 3).equals("Three Thousand"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(4, 3).equals("Four Thousand"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(5, 3).equals("Five Thousand"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(6, 3).equals("Six Thousand"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(7, 3).equals("Seven Thousand"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(8, 3).equals("Eight Thousand"));
-        assertTrue(NumberToWordConverter.baseTenMultipleToWords(9, 3).equals("Nine Thousand"));
+        assertEquals("One Thousand", NumberToWordConverter.baseTenMultipleToWords(1, 3));
+        assertEquals("Two Thousand", NumberToWordConverter.baseTenMultipleToWords(2, 3));
+        assertEquals("Three Thousand", NumberToWordConverter.baseTenMultipleToWords(3, 3));
+        assertEquals("Four Thousand", NumberToWordConverter.baseTenMultipleToWords(4, 3));
+        assertEquals("Five Thousand", NumberToWordConverter.baseTenMultipleToWords(5, 3));
+        assertEquals("Six Thousand", NumberToWordConverter.baseTenMultipleToWords(6, 3));
+        assertEquals("Seven Thousand", NumberToWordConverter.baseTenMultipleToWords(7, 3));
+        assertEquals("Eight Thousand", NumberToWordConverter.baseTenMultipleToWords(8, 3));
+        assertEquals("Nine Thousand", NumberToWordConverter.baseTenMultipleToWords(9, 3));
     }
 
 }
