389a8e6909b7c3636b36a1188a6c9733f75a97cc
Anwar A. Ruff <anwarruff@gmail.com>
2015-01-17 23:19:21 -0500

    Completed implementation of the factionalPortionInWords method
    method doc blocks were also added

--- src/main/java/edu/nyu/cess/experiment/edu/nyu/cess/util/numeric/NumberToWordConverter.java
+++ src/main/java/edu/nyu/cess/experiment/edu/nyu/cess/util/numeric/NumberToWordConverter.java
@@ -11,15 +11,33 @@ public class NumberToWordConverter
 
     private static final int BASE = 10;
 
-
     private static final double MAX_WHOLE_NUMBER = 10000.0;
     private static final double MIN_WHOLE_NUMBER = 0.0;
 
+    private static final int MAX_FRACTION_PLACE_VALUE = 100;
+
     private static final String[] ones = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
     private static final String[] tens = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    private static final String[] baseTen = {"", "", "Hundred", "Thousand", "Million"};
+    private static final String[] powerTenInWords = {"", "", "Hundred", "Thousand", "Million"};
 
-    public static String wholePortionToWords(double number)
+    /**
+     * Returns the fractional part of the number in words.
+     *
+     * @param number
+     * @return
+     */
+    public static String fractionalPortionInWords(double number)
+    {
+        double fractionalNumber = number - Math.floor(number);
+        return wholePortionInWords(Math.floor(fractionalNumber*MAX_FRACTION_PLACE_VALUE));
+    }
+
+    /**
+     * Returns the whole part of the number in words.
+     * @param number
+     * @return
+     */
+    public static String wholePortionInWords(double number)
     {
         if (number >= MAX_WHOLE_NUMBER || number <= MIN_WHOLE_NUMBER) {
             return "";
@@ -35,10 +53,15 @@ public class NumberToWordConverter
             }
             tempNumber -= coefficient*powerOfTen;
         }
-
         return wholePartInWords.trim();
     }
 
+    /**
+     * Converts a base ten number (c*10^e) to words using the base 10 coefficient and exponent value.
+     * @param coefficient base 10 coefficient (c*10)
+     * @param exponent base 10 exponent (c*10^e)
+     * @return
+     */
     public static String baseTenMultipleToWords(int coefficient, int exponent)
     {
         if (exponent == ONES_EXPONENT) {
@@ -48,7 +71,7 @@ public class NumberToWordConverter
             return tens[coefficient];
         }
         else if(exponent == HUNDREDTHS_EXPONENT || exponent == THOUSANDTHS_EXPONENT) {
-            return ones[coefficient] + " " + baseTen[exponent];
+            return ones[coefficient] + " " + powerTenInWords[exponent];
         }
 
         return "";
