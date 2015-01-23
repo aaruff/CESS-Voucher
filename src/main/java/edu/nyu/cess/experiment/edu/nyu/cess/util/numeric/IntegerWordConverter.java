544d5796333cf5c55387074a4dadd24e4f701c2e
Anwar A. Ruff <anwarruff@gmail.com>
2015-01-22 23:08:00 -0500

    renamed compound digit test method

--- src/main/java/edu/nyu/cess/experiment/edu/nyu/cess/util/numeric/IntegerWordConverter.java
+++ src/main/java/edu/nyu/cess/experiment/edu/nyu/cess/util/numeric/IntegerWordConverter.java
@@ -24,8 +24,8 @@ public class IntegerWordConverter
         final int MAX_POSITION = 3, MIN_POSITION = 1;
         for (int tempNumber = number, position = MAX_POSITION; position >= MIN_POSITION; --position)
         {
-            if(isDependentPlaceValueNumber(position, tempNumber)) {
-                translation += getDependentPlaceValueNumberAsWord(tempNumber);
+            if(isNumberTwoDigitCompoundWord(position, tempNumber)) {
+                translation += getDependentPlaceValueAsWord(tempNumber);
                 break;
             }
 
@@ -76,16 +76,16 @@ public class IntegerWordConverter
     }
 
     /**
-     * Returns true if the number cannot be represented in words with only one place value, and is also not divisible
-     * by ten (i.e. numbers between 11 and 19).
+     * Returns true if the number is a two digit compound word, otherwise false.
      *
-     * For example, 1 in 12 is a dependent place value, because it can't be described in words without inspecting
-     * the number in the ones place, namely 2.
+     * Two digit compound words cannot be translated to words unless two position values are known, namely
+     * positions 2 and 1. All two digit compound words are between 11 and 19.
+     *
      * @param position the position to inspect
      * @param number the number
      * @return boolean boolean
      */
-    private static boolean isDependentPlaceValueNumber(int position, int number)
+    private static boolean isNumberTwoDigitCompoundWord(int position, int number)
     {
         final int HUNDREDTHS_PLACE = 2;
         if (position != HUNDREDTHS_PLACE) {
@@ -105,7 +105,7 @@ public class IntegerWordConverter
      * @param number the number
      * @return string string
      */
-    private static String getDependentPlaceValueNumberAsWord(int number)
+    private static String getDependentPlaceValueAsWord(int number)
     {
         int onesValue = number - BASE;
 
@@ -113,14 +113,18 @@ public class IntegerWordConverter
         return TEENS[onesValue];
     }
 
+    private static String getOnesPlaceValueAsWord()
+
     private static String getBaseTenNumberAsWord(int position, int number)
     {
+        final String NO_TRANSLATION = "";
+
         int coefficient = getPositionValue(position, number);
 
-        final String[] ONES = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+        final String[] ONES = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
         final int ONES_EXPONENT = 1;
         if (position == ONES_EXPONENT) {
-            return ONES[coefficient];
+            return (coefficient == 0) ? NO_TRANSLATION : ONES[coefficient];
         }
 
         final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
