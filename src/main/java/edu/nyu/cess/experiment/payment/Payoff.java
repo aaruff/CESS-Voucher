39a4f57a40d6c7064c4e61c8a2d13bdc7b728de5
Anwar A. Ruff <anwarruff@gmail.com>
2015-01-26 22:59:02 -0500

    Updated the payoff constructor to reflect refactoring made to the UnitedStatesDollar class

--- src/main/java/edu/nyu/cess/experiment/payment/Payoff.java
+++ src/main/java/edu/nyu/cess/experiment/payment/Payoff.java
@@ -3,17 +3,20 @@ package edu.nyu.cess.experiment.payment;
 import edu.nyu.cess.experiment.payment.currency.UnitedStatesDollar;
 
 public class Payoff {
-	private UnitedStatesDollar payoff;
+	private UnitedStatesDollar usd;
 
-	public Payoff(String dollarAmount){
-		this.payoff = new UnitedStatesDollar(Double.valueOf(dollarAmount));
+	public Payoff(String dollarAmount)
+	{
+		String[] splitAmount = dollarAmount.toString().split("\\.");
+		final int DOLLAR = 0, CENTS = 1, BASE = 10;
+		usd = new UnitedStatesDollar(Integer.parseInt(splitAmount[DOLLAR], BASE), Integer.parseInt(splitAmount[CENTS], BASE));
 	}
 	
-	public String getPayoff(){
-		return this.payoff.getValueWithCurrencySymbol();
+	public String getUsd(){
+		return this.usd.getValueWithCurrencySymbol();
 	}
 	
 	public String getPayoffInWords(){
-		return this.payoff.getInWords();
+		return this.usd.getInWords();
 	}
 }
