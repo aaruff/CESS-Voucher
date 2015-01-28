c7e1439fb979e4be6d1839ca19c881dbd8ecdc74
Anwar A. Ruff <anwarruff@gmail.com>
2015-01-27 20:50:37 -0500

    Added test cases

--- src/test/java/edu/nyu/cess/experiment/payment/currency/UnitedStatesDollarTest.java
+++ src/test/java/edu/nyu/cess/experiment/payment/currency/UnitedStatesDollarTest.java
@@ -8,6 +8,13 @@ public class UnitedStatesDollarTest extends TestCase
     {
         UnitedStatesDollar usd = new UnitedStatesDollar(22, 50);
         assertEquals("Twenty Two Dollars And Fifty Cents", usd.getInWords());
+        usd.setAmount(0, 0);
+        assertEquals("", usd.getInWords());
+        usd.setAmount(0, 10);
+        assertEquals("Ten Cents", usd.getInWords());
+        usd.setAmount(0, 1);
+        assertEquals("One Cent", usd.getInWords());
+        //TODO: Test cent values > 99 (i.e., out of range)
     }
 
 }
